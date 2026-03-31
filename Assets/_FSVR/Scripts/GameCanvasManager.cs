40727886690de8c7da023f38a425b52bde099b5b
tangentlab <guidewire@gmail.com>
2026-03-30 20:32:39 -0600

    create game canvas manager

---
+++ Assets/_FSVR/Scripts/GameCanvasManager.cs
@@ -0,0 +1,39 @@
+using System;
+using DG.Tweening;
+using FSVR;
+using UnityEngine;
+
+public class GameCanvasManager : MonoBehaviour
+{
+    [SerializeField] private GameObject welcomeItmes;
+    [SerializeField] private ScrabbleManager scrabbleManager;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        RevealWelcomeItems();
+        Debug.Log("--GameCanvasManager OnEnable");
+        
+        
+    }
+
+    public void RevealWelcomeItems()
+    {
+        //DOTween.To(() => welcomeItmes.transform.localScale, x => welcomeItmes.transform.localScale = x, new Vector3(1, 1, 1), 0.5f);
+        
+    }
+
+    public void StartScrabble()
+    {
+        //welcomeItmes.SetActive(false);
+        scrabbleManager.StartGame();
+        
+    }
+    
+    
+}
