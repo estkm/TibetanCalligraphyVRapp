21b6ce5a82282e8379ac35a76f632041eb5b2440
tangentlab <guidewire@gmail.com>
2026-04-07 15:42:24 -0600

    Update ScrabbleManager.cs

--- Assets/_FSVR/Scripts/ScrabbleManager.cs
+++ Assets/_FSVR/Scripts/ScrabbleManager.cs
@@ -1,4 +1,5 @@
 
+using System.Text;
 using DG.Tweening;
 using UnityEngine;
 
@@ -10,22 +11,21 @@ namespace FSVR
         [SerializeField] private GameObject[] houseSymbols;
         [SerializeField] private GameObject[] houseTiles;
         
+        [SerializeField] private TileSlot[] slots;
+        [SerializeField] private string targetWord = "CAT";
+        
         private FSManager _fsManager;
-
-        //private bool gameEnd = false;
-        //private bool gameStarted = false;
         
         //game status enum
         private enum GameStatus { Waiting, Playing, Done}
         private GameStatus gameStatus = GameStatus.Waiting;
         
-        
-        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             _fsManager = FSManager.Instance;
-            _fsManager.TestLoad();
+            //_fsManager.TestLoad();
+            
             house.SetActive(false);
         }
 
@@ -36,10 +36,65 @@ namespace FSVR
             DisplayHouseSymbols();
             
         }
-
-        void CompareSequence()
+        
+        public string GetCurrentWord()
         {
-            
+            var sb = new StringBuilder();
+
+            foreach (var slot in slots)
+            {
+                if (!slot.currentSymbolTile)
+                {
+                    sb.Append("_");
+                }
+                else
+                {
+                    sb.Append(slot.currentSymbolTile.Symbol);
+                }
+            }
+
+            return sb.ToString();
+        }
+        
+        public bool IsCorrectWord()
+        {
+            if (!AreAllSlotsFilled())
+                return false;
+
+            var builtWord = "";
+
+            foreach (TileSlot slot in slots)
+            {
+                builtWord += slot.currentSymbolTile.Symbol;
+            }
+
+            return string.Equals(builtWord, targetWord, System.StringComparison.OrdinalIgnoreCase);
+        }
+        
+        public bool AreAllSlotsFilled()
+        {
+            foreach (TileSlot slot in slots)
+            {
+                if (slot.currentSymbolTile == null)
+                    return false;
+            }
+
+            return true;
+        }
+        
+        public void CheckWord()
+        {
+            var current = GetCurrentWord();
+            Debug.Log("Current sequence: " + current);
+
+            if (IsCorrectWord())
+            {
+                Debug.Log("Correct word!");
+            }
+            else
+            {
+                Debug.Log("Not correct.");
+            }
         }
         
         void DisplayHouseSymbols()
