4cd84de0f11df2b0b7b15a4d05fa8842731b95e7
tangentlab <47337594+tangentlab@users.noreply.github.com>
2026-04-13 16:40:55 -0600

    Update ScrabbleManager.cs

--- Assets/_FSVR/Scripts/ScrabbleManager.cs
+++ Assets/_FSVR/Scripts/ScrabbleManager.cs
@@ -1,120 +1,139 @@
-
+using System;
 using System.Text;
 using DG.Tweening;
 using UnityEngine;
 
 namespace FSVR
 {
-    public class ScrabbleManager : MonoBehaviour
-    {
-        [SerializeField] private GameObject house;
-        [SerializeField] private GameObject[] houseSymbols;
-        [SerializeField] private GameObject[] houseTiles;
-        
-        [SerializeField] private TileSlot[] slots;
-        [SerializeField] private string targetWord = "CAT";
-        
-        private FSManager _fsManager;
-        
-        //game status enum
-        private enum GameStatus { Waiting, Playing, Done}
-        private GameStatus gameStatus = GameStatus.Waiting;
-        
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-            _fsManager = FSManager.Instance;
-            //_fsManager.TestLoad();
-            
-            house.SetActive(false);
-        }
+	public class ScrabbleManager : MonoBehaviour
+	{
+		[SerializeField] private string targetWord = "KHANGA";
 
-        public void StartGame()
-        {
-            gameStatus = GameStatus.Playing;
-            gameObject.SetActive(true);
-            DisplayHouseSymbols();
-            
-        }
-        
-        public string GetCurrentWord()
-        {
-            var sb = new StringBuilder();
+		[SerializeField] private GameObject house;
+		[SerializeField] private GameObject[] houseSymbols;
+		[SerializeField] private GameObject[] houseTiles;
 
-            foreach (var slot in slots)
-            {
-                if (!slot.currentSymbolTile)
-                {
-                    sb.Append("_");
-                }
-                else
-                {
-                    sb.Append(slot.currentSymbolTile.Symbol);
-                }
-            }
+		[SerializeField] private TileSlot[] slots;
 
-            return sb.ToString();
-        }
-        
-        public bool IsCorrectWord()
-        {
-            if (!AreAllSlotsFilled())
-                return false;
+		private FSManager _fsManager;
 
-            var builtWord = "";
+		//game status enum
+		private enum GameStatus
+		{ Waiting, Playing, Done }
 
-            foreach (TileSlot slot in slots)
-            {
-                builtWord += slot.currentSymbolTile.Symbol;
-            }
+		private GameStatus gameStatus = GameStatus.Waiting;
 
-            return string.Equals(builtWord, targetWord, System.StringComparison.OrdinalIgnoreCase);
-        }
-        
-        public bool AreAllSlotsFilled()
-        {
-            foreach (TileSlot slot in slots)
-            {
-                if (slot.currentSymbolTile == null)
-                    return false;
-            }
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		private void Start ()
+		{
+			_fsManager = FSManager.Instance;
+			//_fsManager.TestLoad();
 
-            return true;
-        }
-        
-        public void CheckWord()
-        {
-            var current = GetCurrentWord();
-            Debug.Log("Current sequence: " + current);
+			house.SetActive(false);
+		}
 
-            if (IsCorrectWord())
-            {
-                Debug.Log("Correct word!");
-            }
-            else
-            {
-                Debug.Log("Not correct.");
-            }
-        }
-        
-        void DisplayHouseSymbols()
-        {
-            foreach (var houseSymbol in houseSymbols)
-            {
-                houseSymbol.SetActive(true);
-            }
-        }
+		public void StartGame ()
+		{
+			gameStatus = GameStatus.Playing;
+			gameObject.SetActive(true);
+			DisplayHouseSymbols();
+		}
 
-        void DisplayHouse()
-        {
-            house.SetActive(true);
-        }
+		public string BuildCurrentWord ()
+		{
+			var sb = new StringBuilder();
 
-        void FinishGame()
-        {
-            gameStatus = GameStatus.Done;
-            DisplayHouse();
-        }
-        
-    }
+			foreach (var slot in slots)
+			{
+				if (!slot.currentTile)
+				{
+					sb.Append("_");
+				}
+				else
+				{
+					sb.Append(slot.currentTile.Symbol);
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		public bool IsCorrectWord ()
+		{
+			if (!AreAllSlotsFilled())
+				return false;
+
+			var builtWord = "";
+
+			foreach (TileSlot slot in slots)
+			{
+				builtWord += slot.currentTile.Symbol;
+			}
+
+			return string.Equals(builtWord, targetWord, System.StringComparison.OrdinalIgnoreCase);
+		}
+
+		public bool AreAllSlotsFilled ()
+		{
+			foreach (TileSlot slot in slots)
+			{
+				if (slot.currentTile == null)
+					return false;
+			}
+
+			return true;
+		}
+
+		public void CheckWord ()
+		{
+			var currentWord = BuildCurrentWord();
+			Debug.Log("-- Current sequence: " + currentWord);
+
+			if (currentWord == targetWord)
+			{
+				Debug.Log("-- Correct word!");
+				OnSolved();
+			}
+			else
+			{
+				Debug.Log("-- Not correct.");
+			}
+		}
+
+		private void DisplayHouseSymbols ()
+		{
+			foreach (var houseSymbol in houseSymbols)
+			{
+				houseSymbol.SetActive(true);
+			}
+		}
+
+		private void DisplayHouse ()
+		{
+			house.SetActive(true);
+			house.transform.DOScale(house.transform.localScale, 1f).From(0f).SetEase(Ease.InOutExpo);
+			house.transform.DORotateQuaternion(Quaternion.Euler(0f, 360f, 0f), 1f).From().SetEase(Ease.InOutExpo);
+		}
+
+		private void OnSolved ()
+		{
+			gameStatus = GameStatus.Done;
+			//hide tiles and slots
+			HideTiles();
+			DisplayHouse();
+		}
+
+		private void HideTiles ()
+		{
+			foreach (var tile in houseTiles)
+			{
+				tile.SetActive(false);
+			}
+
+			foreach (var slot in slots)
+			{
+				slot.gameObject.SetActive(false);
+			}
+		}
+	}
 }
