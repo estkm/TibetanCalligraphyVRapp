3b0ced561b89934c1d782633eaab470956d0a82e
tangentlab <47337594+tangentlab@users.noreply.github.com>
2026-04-16 17:20:21 -0600

    Update GameCanvasManager.cs

--- Assets/_FSVR/Scripts/GameCanvasManager.cs
+++ Assets/_FSVR/Scripts/GameCanvasManager.cs
@@ -1,48 +1,41 @@
-
 using FSVR;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class GameCanvasManager : MonoBehaviour
 {
-    [FormerlySerializedAs("welcomeItmes")] [SerializeField] private GameObject welcomeHolder;
-    [SerializeField] private GameObject baseGame;
-    
-    [Header("Scrabble")]
-    [SerializeField] private GameObject scrabbleCanvas;
-    [SerializeField] private ScrabbleManager scrabbleManager;
-    [SerializeField] private GameObject scrabbleButton;
-    
-    private FSManager _FSManager;
-    
-    private void Awake()
-    {
-        _FSManager = FSManager.Instance;
-       
-        scrabbleButton.SetActive(_FSManager.DevMode);
-        
-    }
-    
-    private void OnEnable()
-    {
-        RevealWelcomeItems();
-        
-    }
+	[FormerlySerializedAs("welcomeItmes")][SerializeField] private GameObject welcomeHolder;
+	[SerializeField] private GameObject baseGame;
 
-    private void RevealWelcomeItems()
-    {
-        welcomeHolder.SetActive(true);
-        Debug.Log("--GameCanvasManager RevealWelcomeItems");
-        
-    }
+	[Header("Scrabble")]
+	[SerializeField] private GameObject scrabbleCanvas;
 
-    public void StartScrabble()
-    {
-        
-        baseGame.SetActive(true);
-        scrabbleManager.StartGame();
-        
-    }
-    
-    
+	[SerializeField] private ScrabbleManager scrabbleManager;
+	[SerializeField] private GameObject scrabbleButton;
+
+	private FSManager _FSManager;
+
+	private void Awake ()
+	{
+		_FSManager = FSManager.Instance;
+
+		scrabbleButton.SetActive(_FSManager.DevMode);
+	}
+
+	private void OnEnable ()
+	{
+		RevealWelcomeItems();
+	}
+
+	private void RevealWelcomeItems ()
+	{
+		welcomeHolder.SetActive(true);
+		Debug.Log("--GameCanvasManager RevealWelcomeItems");
+	}
+
+	public void StartScrabble ()
+	{
+		baseGame.SetActive(true);
+		scrabbleManager.StartGame();
+	}
 }
